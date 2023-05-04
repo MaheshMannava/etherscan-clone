e0ee090d8b6698223796bea22b3fc4b1afafc0d4
___Maheshhh <71592960+MaheshMannava@users.noreply.github.com>
2023-05-04 12:59:43 +0530

    final-updt

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 5001;
+const port = "https://etherscan-clone.onrender.com";
 const Moralis = require("moralis").default;
 const cors = require("cors");
 
